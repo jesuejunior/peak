803ce568ef32988266e38fd3a4436028e34d2aff
Jesue Junior <jesuesousa@gmail.com>
2016-08-11 14:56:23 -0400

    Improving project structure

---
+++ src/components/app/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class PeakApp extends React.Component {
+    render(){
+        return <h1> INDEX </h1>    
+    }    
+    
+}
+
+ReactDOM.render(<PeakApp />, document.getElementById('peakApp'));
