298051cefcc61db9163d0c20f6c4dd7761aa8a79
Jesue Junior <jesuesousa@gmail.com>
2016-08-11 13:48:22 -0400

    Starting reactJS project

---
+++ menu.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Menu extends React.Component {
+    render(){
+        return <h1> Menu </h1>
+    }
+
+}
+
+ReactDOM.render(<Menu />, document.getElementById('menu'));
