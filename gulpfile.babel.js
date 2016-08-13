fca47baade16496e9c3f3cb56e9bfccb3c6f9442
Jesue Junior <jesuesousa@gmail.com>
2016-08-13 16:17:35 -0300

    tasks by gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,52 +1,29 @@
 'use strict';
 
 var gulp         = require('gulp');
-var babel        = require('gulp-babel');
 var browserSync  = require('browser-sync');
 var concat       = require('gulp-concat');
 var eslint       = require('gulp-eslint');
 var filter       = require('gulp-filter');
-var newer        = require('gulp-newer');
-var notify       = require('gulp-notify');
-var plumber      = require('gulp-plumber');
 var reload       = browserSync.reload;
 var css          = require('gulp-css');
-var sourcemaps   = require('gulp-sourcemaps');
 var rev          = require('gulp-rev');
 var revReplace   = require('gulp-rev-replace');
 var uglify       = require('gulp-uglify');
 var csso         = require('gulp-csso');
 var useref       = require('gulp-useref');
 var clean        = require('gulp-clean');
-var babelify = require('babelify');
-var source = require('vinyl-source-stream');
+var babelify     = require('babelify');
+var source       = require('vinyl-source-stream');
+var buffer       = require('vinyl-buffer');
+var sourcemaps   = require('gulp-sourcemaps');
+var browserify   = require('browserify');
+var watchify     = require('watchify');
 
-
-var onError = function(err) {
-  notify.onError({
-    title:    "Error",
-    message:  "<%= error %>",
-  })(err);
-  this.emit('end');
-};
-
-var plumberOptions = {
-  errorHandler: onError,
-};
-
-var jsFiles = {
-  vendor: [
-
-  ],
-  source: [
-    'src/components/**/*.js',
-    'src/components/**/*.jsx',
-  ]
-};
 
 // Lint JS/JSX files
 gulp.task('eslint', function() {
-  return gulp.src(jsFiles.source)
+  return gulp.src('src/**/*.js*')
     .pipe(eslint({
       baseConfig: {
         "parseOptions": {
@@ -58,28 +35,28 @@ gulp.task('eslint', function() {
     .pipe(eslint.failAfterError());
 });
 
-// Concatenate jsFiles.vendor and jsFiles.source into one JS file.
-// Run copy-react and eslint before concatenating
-gulp.task('concat', ['eslint'], function() {
-  return gulp.src(jsFiles.vendor.concat(jsFiles.source))
-    .pipe(sourcemaps.init())
-    // .pipe(babel({ presets: ['es2015'] }))
-    // .pipe(concat('app.js'))
+gulp.task('jsx', ['eslint'], function() {
+    var b =  watchify(
     browserify({
-      entries: 'src/index.js',
-      extensions: ['.js'],
-      debug: true
+      entries: ['src/app.js'],
+      debug: true,
+      extensions: [' ', 'js', 'jsx']
     })
-    .transform(babelify)
-    .bundle()
+    .transform(babelify.configure({
+      presets: ["es2015", 'react']
+    })))
+    return b.bundle()
+    .on('error', function(err) { console.error(err); this.emit('end'); })
     .pipe(source('app.js'))
-    // .pipe(uglify())
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('public'));
+    // .pipe(uglify())
+    .pipe(gulp.dest('./public'));
 });
 
 gulp.task("index", function() {
-  var jsFilter = filter("src/**/*.js", { restore: true });
+  var jsFilter = filter("public/app.js", { restore: true });
   var cssFilter = filter("src/**/*.css", { restore: true });
   var indexHtmlFilter = filter(['src/**/*', '!**/index.html'], { restore: true });
 
@@ -105,7 +82,7 @@ gulp.task('clean', function () {
 
 // Watch JS/JSX and Sass files
 gulp.task('watch', function() {
-  gulp.watch('src/**/*.{js,jsx,html}', ['concat', 'index']);
+  gulp.watch('src/**/*.{js,jsx,html}', ['jsx', 'index']);
   gulp.watch('css/**/*.css', ['css']);
 });
 
@@ -122,5 +99,5 @@ gulp.task('browsersync', function() {
   });
 });
 
-gulp.task('build', ['concat', 'index']);
+gulp.task('build', ['jsx', 'index']);
 gulp.task('default', ['build', 'browsersync', 'watch']);
