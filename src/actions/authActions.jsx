659a02ab569346abce9a2dc1e40c717dafda5e7f
Gabriel Seixas <gseixas13@hotmail.com>
2016-09-16 14:58:37 -0300

    Removing auth login from user

---
+++ src/actions/authActions.jsx
@@ -0,0 +1,14 @@
+import AppDispatcher from '../dispatcher/AppDispatcher.jsx';
+import AuthConstants from '../constants/AuthConstants.jsx';
+
+export function loginUserAction() {
+
+}
+
+export function logoutUserAction() {
+
+}
+
+export function loggedUserAction() {
+    
+}
