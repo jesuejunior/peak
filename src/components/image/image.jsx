1a44dc4bf4665ce4800234ca01f1c93888fb46b6
Arlindo Pereira <arlindosaraivapereira@gmail.com>
2016-10-07 17:54:43 -0300

    Image component skeleton

---
+++ src/components/image/image.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default function Image (props) {
+  return(
+    <div className="image">
+      <div className="content">
+        image
+      </div>
+    </div>
+  );
+}
